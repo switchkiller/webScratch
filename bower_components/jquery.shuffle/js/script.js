86b036632ee746f6b7f45717a752984786ba81a9
switchkiller <emailbarung@gmail.com>
2015-12-11 02:29:45 +0545

    Added percentage meter, added shuffling effect JS

---
+++ bower_components/jquery.shuffle/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+
+	// container is the DOM element;
+	// userText is the textbox
+
+	var container = $("#shuffle")
+		userText = $('#userText');
+
+	// Shuffle the contents of container
+	shuffle.shuffleLetters();
+});
